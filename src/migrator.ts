eba1f93f0d77e0315430874a6adbb0d1b9778b27
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2023-04-27 17:59:00 +0200

    Add dependencies chalk and cli-progress to package.json and tsconfig.json updates. Improve console logging and add progress bars

--- src/migrator.ts
+++ src/migrator.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as cheerio from "cheerio";
+import chalk from "chalk";
+import cliProgress from "cli-progress";
 import { Converter } from "./converter";
 import { handleError, logger } from "./logger";
 
@@ -16,6 +18,9 @@ class Migrator {
     logger.info("Starting migration for file: %s", inputFilename);
 
     const html = await fs.promises.readFile(input, "utf8");
+
+    // Load the HTML into Cheerio and disable XML encoding
+    // otherwise attributenames will be lowercased
     const $ = cheerio.load(
       html,
       { xml: { decodeEntities: false, lowerCaseAttributeNames: false } },
@@ -29,8 +34,11 @@ class Migrator {
 
     logger.debug("Found %i elements", elements.length);
 
+    const fileProgressBar = createFileProgressBar(inputFilename);
+    fileProgressBar.start(elements.length, 0);
+
     // Iterate through the elements and perform the conversion
-    elements.each((_, element) => {
+    elements.each((index, element) => {
       const el = $(element);
       const attrs = el.attr();
 
@@ -40,11 +48,15 @@ class Migrator {
         logger.debug("Can convert: %s", this.converter.canConvert(attribute));
         if (this.converter.canConvert(attribute)) {
           this.converter.convert(attribute, value, el);
+
           el.removeAttr(attribute);
-          // el.addClass(convertedValue);
         }
       }
+
+      fileProgressBar.update(index + 1);
     });
+
+    fileProgressBar.stop();
 
     const migratedHtml = $.html({ xmlMode: false });
 
@@ -64,21 +76,30 @@ class Migrator {
     const files = await fs.promises.readdir(inputFolder);
     const totalFiles = files.length;
     logger.info(
-      "Starting migration for %i files in folder: %s",
+      chalk.yellow("Starting migration for %i files in folder: %s"),
       totalFiles,
       inputFolder
     );
 
+    // Starten Sie den Fortschrittsbalken
+    this.progressBar.start(totalFiles, 0);
+
     await Promise.all(
-      files.map(async (file) => {
+      files.map(async (file, index) => {
         const inputFile = path.join(inputFolder, file);
         const outputFile = path.join(outputFolder, file);
         await this.migrateFile(inputFile, outputFile);
-        logger.info("Migration completed for file: %s", file);
+        logger.info(chalk.green("Migration completed for file: %s"), file);
+
+        // Aktualisieren Sie den Fortschrittsbalken
+        this.progressBar.update(index + 1);
       })
     );
 
-    logger.info("Migration completed for folder: %s", inputFolder);
+    // Beenden Sie den Fortschrittsbalken
+    this.progressBar.stop();
+
+    logger.info(chalk.green("Migration completed for folder: %s"), inputFolder);
   }
 
   public async migrate(input: string, output: string): Promise<void> {
@@ -99,6 +120,34 @@ class Migrator {
       handleError(`Error while processing the input path: ${input}`, error);
     }
   }
+
+  private progressBar = new cliProgress.SingleBar({
+    format:
+      chalk.yellow("Migrating Files") +
+      " |" +
+      chalk.cyan("{bar}") +
+      "| {percentage}% || {value}/{total} Files",
+    barCompleteChar: "\u2588",
+    barIncompleteChar: "\u2591",
+    hideCursor: true,
+  });
+}
+
+function createFileProgressBar(fileName: string): cliProgress.SingleBar {
+  const fileProgressBar = new cliProgress.SingleBar({
+    format:
+      chalk.blue(`Migrating ${fileName}`) +
+      " |" +
+      chalk.cyan("{bar}") +
+      "| {percentage}% || {value}/{total} Elements",
+    barCompleteChar: "\u2588",
+    barIncompleteChar: "\u2591",
+    hideCursor: true,
+  });
+
+  return fileProgressBar;
 }
 
 export { Migrator };
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
