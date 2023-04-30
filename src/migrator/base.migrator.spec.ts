5fe08ccbfa285f0e993220f14be2208f765d375b
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2023-04-30 16:44:14 +0200

    implemented some tests for migrators

---
+++ src/migrator/base.migrator.spec.ts
@@ -0,0 +1,52 @@
+import { BaseMigrator } from "./base.migrator";
+import { IConverter } from "../converter/converter";
+import { Observer } from "./observer/migrator.observer";
+
+class TestMigrator extends BaseMigrator {
+  async migrate(): Promise<void> {
+    this.notifyObservers("testEvent", { message: "Test message" });
+  }
+
+  getObserverCount(): number {
+    return this.observers.length;
+  }
+}
+
+describe("BaseMigrator", () => {
+  let converter: IConverter;
+  let migrator: TestMigrator;
+
+  beforeEach(() => {
+    converter = {
+      canConvert: jest.fn().mockReturnValue(false),
+      convert: jest.fn(),
+      getAllAttributes: jest.fn().mockReturnValue([]),
+    } as unknown as IConverter;
+
+    migrator = new TestMigrator(converter);
+  });
+
+  test("addObserver and removeObserver", () => {
+    const observer: Observer = {
+      update: jest.fn(),
+    };
+
+    migrator.addObserver(observer);
+    expect(migrator.getObserverCount()).toBe(1);
+
+    migrator.removeObserver(observer);
+    expect(migrator.getObserverCount()).toBe(0);
+  });
+
+  test("notifyObservers", async () => {
+    const observer: Observer = {
+      update: jest.fn(),
+    };
+    migrator.addObserver(observer);
+
+    await migrator.migrate();
+    expect(observer.update).toHaveBeenCalledWith("testEvent", {
+      message: "Test message",
+    });
+  });
+});
---
+++ src/migrator/file.migrator.spec.ts
@@ -0,0 +1,62 @@
+import { FileMigrator } from "./file.migrator";
+import { IConverter } from "../converter/converter";
+
+import * as fs from "fs-extra";
+import * as path from "path";
+
+const readFileMock = jest.fn();
+const writeFileMock = jest.fn();
+const mkdirMock = jest.fn();
+
+jest.mock("fs-extra");
+jest.mock("path");
+
+const mockedFs = fs as jest.Mocked<typeof fs>;
+const mockedPath = path as jest.Mocked<typeof path>;
+
+describe("FileMigrator", () => {
+  let converter: IConverter;
+  let fileMigrator: FileMigrator;
+  const input = "input.html";
+  const output = "output.html";
+
+  beforeEach(() => {
+    converter = {
+      canConvert: jest.fn().mockReturnValue(true),
+      convert: jest.fn(),
+      getAllAttributes: jest.fn().mockReturnValue(["fxFlex"]),
+    } as unknown as IConverter;
+
+    fileMigrator = new FileMigrator(converter, input, output);
+
+    jest.spyOn(mockedFs.promises, "readFile").mockImplementation(readFileMock);
+    jest
+      .spyOn(mockedFs.promises, "writeFile")
+      .mockImplementation(writeFileMock);
+    jest.spyOn(mockedFs.promises, "mkdir").mockImplementation(mkdirMock);
+
+    readFileMock.mockImplementation(() =>
+      Promise.resolve('<div fxFlex="100%"></div>')
+    );
+    writeFileMock.mockImplementation(() => Promise.resolve(undefined));
+    mkdirMock.mockImplementation(() => Promise.resolve(undefined));
+
+    mockedPath.dirname.mockReturnValue("path/to/output");
+    mockedPath.basename.mockImplementation((filePath) => filePath);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("migrate() should call converter methods and read/write files", async () => {
+    await fileMigrator.migrate();
+
+    expect(converter.getAllAttributes).toHaveBeenCalled();
+    expect(converter.canConvert).toHaveBeenCalledWith("fxFlex", false);
+    expect(converter.convert).toHaveBeenCalled();
+
+    expect(mockedFs.promises.readFile).toHaveBeenCalledWith(input, "utf8");
+    expect(mockedFs.promises.writeFile).toHaveBeenCalled();
+  });
+});
