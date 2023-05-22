5c6449744c7033bd3d6f4e5283fc500a5ba6f7a5
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2023-05-22 23:56:54 +0200

    reworked folder migrator to avoid stackoverflow error as well as test cases

---
+++ src/lib/stack.spec.ts
@@ -0,0 +1,61 @@
+import { Stack } from './stack';
+
+describe('Stack', () => {
+  let stack: Stack<number>;
+
+  beforeEach(() => {
+    stack = new Stack<number>();
+  });
+
+  it('should create an empty stack', () => {
+    expect(stack.size()).toBe(0);
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('should push items to the stack', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.size()).toBe(2);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+  it('should pop items from the stack', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.size()).toBe(0);
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('should peek at the top item of the stack', () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.peek()).toBe(2);
+    expect(stack.size()).toBe(2);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+  it('should throw an error when popping from an empty stack', () => {
+    expect(() => stack.pop()).toThrowError('Cannot pop from an empty stack');
+  });
+
+  it('should throw an error when peeking at an empty stack', () => {
+    expect(() => stack.peek()).toThrowError('Cannot peek at an empty stack');
+  });
+
+  it('should throw an error when the stack has reached max capacity', () => {
+    const capacity = 2;
+    stack = new Stack<number>([], capacity);
+
+    stack.push(1);
+    stack.push(2);
+
+    expect(() => stack.push(3)).toThrowError(
+      'Stack has reached max capacity, you cannot add more items',
+    );
+  });
+});
--- src/migrator/folder.migrator.spec.ts
+++ src/migrator/folder.migrator.spec.ts
@@ -1,53 +1,106 @@
 import * as fs from 'fs-extra';
-import * as path from 'path';
 import { IConverter } from '../converter/converter';
 import { FolderMigrator } from './folder.migrator';
-import { Observer } from './observer/migrator.observer';
-
-function createTestFolders(inputFolderPath: string): void {
-  fs.ensureDirSync(path.join(inputFolderPath, 'subfolder1'));
-  fs.ensureFileSync(path.join(inputFolderPath, 'file1.html'));
-  fs.ensureFileSync(path.join(inputFolderPath, 'subfolder1', 'file2.html'));
-}
+import { FileMigrator } from './file.migrator';
+import path from 'path';
+import mockFs from 'mock-fs';
 
 describe('FolderMigrator', () => {
-  let converter: IConverter;
-  let observer: Observer;
-  let inputFolder: string;
-  let outputFolder: string;
+  const mockedConverter: IConverter = {
+    convert: jest.fn(),
+    canConvert: jest.fn().mockReturnValue(true),
+    getAllAttributes: jest.fn().mockReturnValue(['fxFlex']),
+    prepare: jest.fn(),
+  };
+
+  const inputFolder = '/input';
+  const outputFolder = '/output';
+  let folderMigrator: FolderMigrator;
 
   beforeEach(() => {
-    converter = {
-      getAllAttributes: jest.fn().mockReturnValue([]),
-      convert: jest.fn(),
-    } as unknown as IConverter;
-
-    observer = { update: jest.fn() };
-    inputFolder = path.join(__dirname, 'test-input');
-    outputFolder = path.join(__dirname, 'test-output');
-
-    createTestFolders(inputFolder);
+    folderMigrator = new FolderMigrator(
+      mockedConverter,
+      inputFolder,
+      outputFolder,
+    );
   });
 
   afterEach(() => {
-    fs.removeSync(inputFolder);
-    fs.removeSync(outputFolder);
+    mockFs.restore();
   });
 
-  test('Folder migration', async () => {
-    const migrator = new FolderMigrator(converter, inputFolder, outputFolder);
-    migrator.addObserver(observer);
-    await migrator.migrate();
+  it('should process a single file correctly', async () => {
+    mockFs({
+      '/input': {
+        'file1.html': '<div></div>',
+      },
+      '/output': {
+        'file1.html': '<div></div>',
+      },
+    });
 
-    // Check if the update method of the observer has been called
-    expect(observer.update).toHaveBeenCalled();
+    await folderMigrator.migrate();
 
-    // Check if the output folder has been created with the correct structure
-    expect(fs.existsSync(outputFolder)).toBe(true);
-    expect(fs.existsSync(path.join(outputFolder, 'subfolder1'))).toBe(true);
-    expect(fs.existsSync(path.join(outputFolder, 'file1.html'))).toBe(true);
-    expect(
-      fs.existsSync(path.join(outputFolder, 'subfolder1', 'file2.html')),
-    ).toBe(true);
+    await fs.promises.access(path.join(outputFolder, 'file1.html'));
+  });
+
+  it('should process a directory of files correctly', async () => {
+    mockFs({
+      '/input': {
+        'file1.html': '<div></div>',
+        'file2.html': '<div></div>',
+      },
+      '/output': {
+        'file1.html': '<div></div>',
+        'file2.html': '<div></div>',
+      },
+    });
+
+    await folderMigrator.migrate();
+
+    await fs.promises.access(path.join(outputFolder, 'file1.html'));
+    await fs.promises.access(path.join(outputFolder, 'file2.html'));
+  });
+
+  it('should process nested directories correctly', async () => {
+    mockFs({
+      '/input': {
+        dir1: {
+          'file1.html': '<div></div>',
+        },
+        dir2: {
+          'file2.html': '<div></div>',
+        },
+      },
+      '/output': {
+        dir1: {
+          'file1.html': '<div></div>',
+        },
+        dir2: {
+          'file2.html': '<div></div>',
+        },
+      },
+    });
+
+    await folderMigrator.migrate();
+
+    await fs.promises.access(path.join(outputFolder, 'dir1', 'file1.html'));
+    await fs.promises.access(path.join(outputFolder, 'dir2', 'file2.html'));
+  });
+
+  it('should call the convert method of the converter', async () => {
+    const spy = jest.spyOn(FileMigrator.prototype, 'migrate');
+    mockFs({
+      '/input': {
+        'file1.html': '<div></div>',
+      },
+      '/output': {
+        'file1.html': '<div></div>',
+      },
+    });
+
+    await folderMigrator.migrate();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
